8a65471d3ce15ba6ffdc2fb8508f1ee60e5ea700
DevDuque <devgduque@gmail.com>
2024-09-21 11:13:32 -0300

    Initial commit

---
+++ app/src/main/java/br/team/background/ImageDownloader.java
@@ -0,0 +1,21 @@
+package br.team.background;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class ImageDownloader {
+
+    public static Bitmap download(String url) throws IOException {
+        Bitmap bitmap = null;
+
+        InputStream inputStream = new URL(url).openStream();
+        bitmap = BitmapFactory.decodeStream(inputStream);
+        inputStream.close();
+
+        return  bitmap;
+    }
+}
